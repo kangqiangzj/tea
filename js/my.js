d8ab503aba3e74e5741d4e1f7dd5718814da7ec5
zhangjie <1148291351@qq.com>
2018-01-15 13:47:19 +0800

    注册验证

--- js/my.js
+++ js/my.js
@@ -1,4 +1,5 @@
 $(function(){
+//	读取本地存储的用户
 	var user = localStorage.getItem("username");
 	if(user){
 		$(".id").text(user);
@@ -23,6 +24,8 @@ $("#login").on("touchend",function(){
 function _login(){
 	var username = $("#login_username").val(),
 		password = $("#login_password").val();
+		
+	
 	$.ajax({
 		type:"post",
 		url:"http://stuapi.ysd3g.com/api/login",
@@ -39,6 +42,8 @@ function _login(){
 				});
 				$(".lr").remove();
 				window.location.reload();
+			}else{
+				alert("密码错了啊")
 			}
 		}
 	});
@@ -46,32 +51,63 @@ function _login(){
 //register
 function register(){
 	var username = $('#register_username').val(),
-		password = $("#register_password").val();
-	$.ajax({
-		type:"post",
-		url:"http://stuapi.ysd3g.com/api/CreateUser",
-		async:true,
-		data:{loginName:username,pwd:password,token:'aa4ccde8-3b85-476d-b68b-7f78f72e74d1'},
-		success:function(data){
-			var data = JSON.parse(data)
-			if(data.success){
-				$(".login_yemian").css({
-//					"left":0
-					"display":"block"
-					});
-				$(".register_yemian").css({
-//						"left":"100%"
-					"display":"none"
-					});
-				alert("注册成功")
-	//			window.location.reload();
-				$(".lr").remove();
-			}else{
-				alert("注册失败")
+		username_len = username.length,
+		user_first = $("#register_username").val().substr(0,1)
+		password = $("#register_password").val(),
+		repassword = $("#repassword").val();
+	//注册用户信息
+	var user_reg = /^[a-z]\w{3,}$/i,
+		usercheck = false,
+		pwdcheck = false,
+		conpwdcheck = false;
+		if(!user_reg.test(username)){
+//			$(".username").css("color","red");
+			if(!((user_first>="a"&&user_first<="w")||(user_first>="A"&&user_first<="W"))){
+				alert("提示：用户名请用 英文字母开头");
+			}else if(username_len<4){
+				alert("提示：用户名至少四个字符")
 			}
-			
+		}else{
+			usercheck = true;
 		}
-	});
+		//确认密码两次是否驶入一致
+		if(password == ""&&usercheck){
+			alert("大兄弟密码都没输入")
+		}else if(password == repassword){
+			conpwdcheck = true;
+		}else if(password != repassword){
+			alert("两次输入密码不一致！");
+		}
+		//提交信息
+		if(usercheck&&conpwdcheck){
+			$.ajax({
+				type:"post",
+				url:"http://stuapi.ysd3g.com/api/CreateUser",
+				async:true,
+				data:{loginName:username,pwd:password,token:'aa4ccde8-3b85-476d-b68b-7f78f72e74d1'},
+				success:function(data){
+					var data = JSON.parse(data)
+					if(data.success){
+						$(".login_yemian").css({
+		//					"left":0
+							"display":"block"
+							});
+						$(".register_yemian").css({
+		//						"left":"100%"
+							"display":"none"
+							});
+						alert("注册成功")
+						$(".lr").remove();
+					}else{
+						alert("注册失败，大兄弟")
+					}
+					
+				}
+			});
+		}else{
+//			alert("注册失败，信息有误")
+		}
+	
 }
 //退出登录
 $(".opt").on("touchend",function(){
